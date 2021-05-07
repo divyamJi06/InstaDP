8dc8f528a6f86454a73c21ca145ef02e9cf1bbac
Divyam Gupta <74950816+divyamJi06@users.noreply.github.com>
2021-05-08 00:05:32 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,26 @@
+
+def apireturn():
+    API_KEY = "1883558051:AAFJuypWMXfyVuPcBwLhBnMQ-sv8Ksaj_pI"
+    return(API_KEY)
+
+# import instaloader
+
+# test = instaloader.Instaloader()
+# acc=input()
+# test.download_profile(acc, profile_pic_only = True)
+
+# import os
+
+# # print(os.listdir(os.path.join(os.getcwd(),"stan_stole")))
+# path=(os.path.join(os.getcwd(),"stan_stole_06"))
+# path1=""
+# for i in os.listdir(path):
+#     if "jpg" in i or "jpeg" in i:
+#         path1=i
+#         break
+# from PIL import Image
+  
+# # open method used to open different extension image file
+# print(os.path.join(path,path1))
+# im = Image.open(os.path.join(path,path1))
+# im.show()
---
+++ instaa.py
@@ -0,0 +1,29 @@
+import telebot
+import api
+import os
+import instaloader
+from PIL import Image
+
+test = instaloader.Instaloader()
+API_KEY = api.apireturn()
+bot = telebot.TeleBot("1797049159:AAEqWGHLn6qE8oRW_kQaSSxHhWXJkag4PgQ", parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello ...\nEnter the user account")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    
+        acc = message.text
+        test.download_profile(acc, profile_pic_only=True)
+        path = (os.path.join(os.getcwd(), acc))
+        for i in os.listdir(path):
+            if "jpg" in i or "jpeg" in i:
+                path = os.path.join(path, i)
+                break
+        im = Image.open(path)
+        bot.send_photo(message.chat.id, im, "Profile picture")
+
+bot.polling()
